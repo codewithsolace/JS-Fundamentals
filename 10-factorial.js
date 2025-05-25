03e049f3de154973c13adff13ab721f6e3a3bac7
codewithsolace <codewithsolace@gmail.com>
2025-05-25 15:36:39 -0700

    add new file

---
+++ 10-factorial.js
@@ -0,0 +1,9 @@
+function factorial(n) {
+  n = parseInt(n);
+  if (isNaN(n)) return 1;
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
+}
+
+const num = process.argv[2];
+console.log(factorial(num));
