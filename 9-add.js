6441316e9fc28d0f4a78acd1e79777724dbd2f7b
codewithsolace <codewithsolace@gmail.com>
2025-05-25 15:30:46 -0700

    add a new file

---
+++ 9-add.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+  return a + b;
+}
+
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+if (isNaN(num1) || isNaN(num2)) {
+  console.log("Invalid arguments - please provide two integers");
+} else {
+  console.log(add(num1, num2));
+}
