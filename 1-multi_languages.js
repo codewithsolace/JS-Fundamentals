a3556affae38cc4e469e0f75ebdb091a205cf6f1
codewithsolace <codewithsolace@gmail.com>
2025-05-25 14:14:22 -0700

    made some correctoins

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,8 +1,8 @@
-const = line1 = "C is fun";
-const = line2 = "Python is cool";
-const = line3 = "JavaScript is amazing";
-
-
+const line1 = "C is fun";
 console.log(line1);
+
+const line2 = "Python is cool";
 console.log(line2);
+
+const line3 = "JavaScript is amazing";
 console.log(line3);
